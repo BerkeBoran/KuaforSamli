fbbe841a5683938da9d961e6912114fd9a157226
berkeboran <sberkeborans@gmail.com>
2025-05-28 21:36:01 +0300

    add Firebase requirements

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+buildscript {
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("com.google.gms:google-services:4.3.15")
+    }
+}
